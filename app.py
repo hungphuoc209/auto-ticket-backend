585d56089482b071b277b7c361781bb098b054aa
st-phuocnguyen <phuoc.nguyen@supremetech.vn>
2022-06-24 07:17:37 +0700

    feat: config server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
 from flask_cors import CORS, cross_origin
-from flask import request
+import main
+import os
 
 # Khởi tạo Flask Server Backend
 app = Flask(__name__)
@@ -8,50 +9,22 @@ app = Flask(__name__)
 # Apply Flask CORS
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = ''
 
-# http://127.0.0.1/add
-# http://127.0.0.1/minus
-# http://127.0.0.1/multi
-# http://127.0.0.1/div
-
-
-@app.route('/add', methods=['POST', 'GET'])
-@cross_origin(origin='*')
-def add_process():
-    a = int(request.args.get('sothunhat'))
-    b = int(request.args.get('sothuhai'))
-    kq = a + b
-    return 'Kết quả là: ' + str(kq)
-
-
-@app.route('/minus', methods=['POST', 'GET'])
-@cross_origin(origin='*')
-def minus_process():
-    a = int(request.args.get('sothunhat'))
-    b = int(request.args.get('sothuhai'))
-    kq = a - b
-    return 'Kết quả là: ' + str(kq)
-
-
-@app.route('/multi')
-@cross_origin(origin='*')
-def multi_process():
-    return "Hehe"
 
 @app.route('/div', methods=['POST', 'GET'])
 @cross_origin(origin='*')
-def div_process():
-    return "Hàm chia"
+def predict():
+    if request.method == 'POST':
+        image = request.files['image']
+        print(image)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], 'image_recognize.jpg'))
+        return main.predict()
 
-
-@app.route('/viethoa', methods=['POST'])
-@cross_origin(origin='*')
-def viethoa_process():
-    s = request.form.get("chuoiinput")
-    #s = request.args.get("chuoiinput")
-    return s.upper()
+    return ''
 
 
 # Start Backend
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='6868')
+    app.run(host='0.0.0.0',port=5555)
+    print("done")
